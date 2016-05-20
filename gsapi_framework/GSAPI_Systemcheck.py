9b03e88c9772d6947ce76be1a6c2f9af97917bd1
kdesai-gridstore <kdesai@gridstore.com>
2016-05-20 16:48:52 +0530

    vPool API support

--- gsapi_framework/GSAPI_Systemcheck.py
+++ gsapi_framework/GSAPI_Systemcheck.py
@@ -1,27 +1,29 @@
 '''
-Created on May 4, 2016
+Created on May 20, 2016
 
-@author: atripathi
+@author: kdesai
 '''
-import requests
+
 import json
+import time 
 import urllib
+
+import requests
+
 from ApiError import ApiError
 from GSAPI_Base import GSAPI_Base
-import time 
 
-class GSAPI_Storage(GSAPI_Base):
+class GSAPI_Systemcheck(GSAPI_Base):
     '''
     classdocs
     '''
-
-
+    
     def __init__(self, host, port):
         '''
         Constructor
         '''
         GSAPI_Base.__init__(self, host, port)
-    
+        
     def storagenode_get (self):
         api = 'grid/storagenode/get'
         auth_api = self._initurl(api)
@@ -39,10 +41,10 @@ class GSAPI_Storage(GSAPI_Base):
         
         return resp_obj
     
-    def storagenode_count (self):
-        api = 'grid/storagenode/count'
+    def host_get (self):
+        api = 'grid/host/get'
         auth_api = self._initurl(api)
-        print 'getting storage node ', auth_api
+        print 'getting host ', auth_api
         #print 'headers -- ', self._get_authheader()
         resp = requests.get(auth_api,  headers=self._get_authheader())
         if resp.status_code != 200:
@@ -51,42 +53,50 @@ class GSAPI_Storage(GSAPI_Base):
             raise ApiError(resp.status_code, 'POST {} / {}'.format(api, resp.status_code))
         
         resp_obj = resp.json()
-        #print resp_obj['access_token']
-        
-        return resp_obj
-    
-    def storagenode_shutdown (self, node, reboot=False):
-        api = 'grid/storagenode/shutdown'
-        auth_api = self._initurl(api, {'nodeid': node, "reboot": reboot})
-        print 'shutdown storage node ', auth_api
-        #print 'headers -- ', self._get_authheader()
-        resp = requests.post(auth_api,  headers=self._get_authheader())
-        if resp.status_code != 200:
-            print resp
-            #     This means something went wrong.
-            raise ApiError(resp.status_code,'POST {} / {}'.format(api, resp.status_code))
-        
-        resp_obj = resp.json()
+         # pretty printing of json-formatted string
         print json.dumps(resp_obj, sort_keys=True, indent=4)
         
         return resp_obj
+    
+    def configuration_profile_get (self):
+        api = 'grid/profile/configuration/get'
+        auth_api = self._initurl(api)
+        print 'getting configuration profile ', auth_api
+        #print 'headers -- ', self._get_authheader()
+        resp = requests.get(auth_api,  headers=self._get_authheader())
+        if resp.status_code != 200:
+            print resp
+            #     This means something went wrong.
+            raise ApiError(resp.status_code, 'POST {} / {}'.format(api, resp.status_code))
         
-
-if __name__ == "__main__":
-    api = GSAPI_Storage('10.100.37.153', '12309')
+        resp_obj = resp.json()
+         # pretty printing of json-formatted string
+        print json.dumps(resp_obj, sort_keys=True, indent=4)
+        
+        return resp_obj
+    
+    def maskingset_get (self):
+        api = 'grid/maskingset/get'
+        auth_api = self._initurl(api)
+        print 'getting maskingsets ', auth_api
+        #print 'headers -- ', self._get_authheader()
+        resp = requests.get(auth_api,  headers=self._get_authheader())
+        if resp.status_code != 200:
+            print resp
+            #     This means something went wrong.
+            raise ApiError(resp.status_code, 'POST {} / {}'.format(api, resp.status_code))
+        
+        resp_obj = resp.json()
+         # pretty printing of json-formatted string
+        print json.dumps(resp_obj, sort_keys=True, indent=4)
+        
+        return resp_obj
+    
+    
+if __name__ == '__main__':
+    api = GSAPI_Systemcheck('10.100.37.153', '12309')
     print 'GS API Login - ', api.login()
-    nodes = api.storagenode_get()
-    for item in nodes :
-        print 'node - ', item['NodeID'], ' IsSyncOrchestrator -- ', item['IsSyncOrchestrator']
-        if item['IsSyncOrchestrator'] is False:
-            print 'shutting down node', item['NodeID']
-            api.storagenode_shutdown(item['NodeID'], True)
-            print 'sleeping for 60 seconds'
-            time.sleep(30);
-
-    print 'GS API grid/storagenode/get ', api.storagenode_get()
-    print 'GS API grid/storagenode/count ', api.storagenode_count() 
+    nodes = api.maskingset_get()
+   
        
     print 'GS API Logout - ', api.logout()
-    
-    
--- gsapi_framework/GSAPI_vStores.py
+++ gsapi_framework/GSAPI_vStores.py
@@ -1,5 +1,5 @@
 '''
-Created on May 19, 2016
+Created on May 20, 2016
 
 @author: kdesai
 '''
@@ -14,21 +14,15 @@ from ApiError import ApiError
 from GSAPI_Base import GSAPI_Base
 
 
-class GSAPI_vPools(GSAPI_Base):
+class GSAPI_vSstores(GSAPI_Base):
     '''
     classdocs
     '''
     
-    def __init__(self, host, port):
-        '''
-        Constructor
-        '''
-        GSAPI_Base.__init__(self, host, port)
-        
-    def vpool_count (self):
-        api = 'grid/vpool/count'
+    def vstore_get (self):
+        api = 'grid/vstore/get'
         auth_api = self._initurl(api)
-        print 'getting vpool count ', auth_api
+        print 'getting vstores ', auth_api
         #print 'headers -- ', self._get_authheader()
         resp = requests.get(auth_api,  headers=self._get_authheader())
         if resp.status_code != 200:
@@ -37,30 +31,16 @@ class GSAPI_vPools(GSAPI_Base):
             raise ApiError(resp.status_code, 'POST {} / {}'.format(api, resp.status_code))
         
         resp_obj = resp.json()
-        print resp_obj
-        
-        return resp_obj
-        
-    def vpool_get (self):
-        api = 'grid/vpool/get'
-        auth_api = self._initurl(api)
-        print 'getting vPools ', auth_api
-        resp = requests.get(auth_api,  headers=self._get_authheader())
-        if resp.status_code != 200:
-            print resp
-            #     This means something went wrong.
-            raise ApiError(resp.status_code, 'POST {} / {}'.format(api, resp.status_code))
-        
-        resp_obj = resp.json()
-        # pretty printing of json-formatted string
+         # pretty printing of json-formatted string
         print json.dumps(resp_obj, sort_keys=True, indent=4)
         
         return resp_obj
     
-    def vStore_per_host_get (self,hostid ):
-        api = 'grid/host/vstore/get'
-        auth_api = self._initurl(api, {'hostid': hostid})
-        print 'Get vStores per host ', auth_api
+    
+    def hosts_per_vstore_get (self,vstoreid ):
+        api = 'grid/vstore/host/get'
+        auth_api = self._initurl(api, {'vstoreid': vstoreid })
+        print 'Get Hosts per vStore API ', auth_api
         #print 'headers -- ', self._get_authheader()
         resp = requests.get(auth_api,  headers=self._get_authheader())
         if resp.status_code != 200:
@@ -75,12 +55,12 @@ class GSAPI_vPools(GSAPI_Base):
         
         return resp_obj
     
-    def storagenode_per_vpool_get (self,vPoolID):
-        api = 'grid/vpool/storagenode/get'
-        auth_api = self._initurl(api, {'VPoolID': vPoolID})
-        print 'Get Storage Nodes of a vPool ', auth_api
+    def start_vstore_post (self,vstoreid ):
+        api = 'grid/vstore/start'
+        auth_api = self._initurl(api, {'vstoreid': vstoreid })
+        print 'Start vStore API ', auth_api
         #print 'headers -- ', self._get_authheader()
-        resp = requests.get(auth_api,  headers=self._get_authheader())
+        resp = requests.post(auth_api,  headers=self._get_authheader())
         if resp.status_code != 200:
             print resp
             #     This means something went wrong.
@@ -93,9 +73,65 @@ class GSAPI_vPools(GSAPI_Base):
         
         return resp_obj
     
-    def create_vpool_post(self,physicalpoolid ,name,optimizationsetting , qossetting, protectionsetting , grouptype ):
-        api = 'grid/vpool/create'
-        auth_api = self._initurl(api, {'name': name,'physicalpoolid ': physicalpoolid , 'optimizationsetting':optimizationsetting,'qossetting':qossetting,'protectionsetting':protectionsetting,'grouptype':grouptype })
+    
+    def stop_vstore_post (self,vstoreid ):
+        api = 'grid/vstore/stop'
+        auth_api = self._initurl(api, {'vstoreid': vstoreid })
+        print 'Stop vStore API ', auth_api
+        #print 'headers -- ', self._get_authheader()
+        resp = requests.post(auth_api,  headers=self._get_authheader())
+        if resp.status_code != 200:
+            print resp
+            #     This means something went wrong.
+            raise ApiError(resp.status_code,'POST {} / {}'.format(api, resp.status_code))
+        
+        resp_obj = resp.json()
+        
+        # pretty printing of json-formatted string
+        print json.dumps(resp_obj, sort_keys=True, indent=4)
+        
+        return resp_obj
+    
+    def remove_vstore_post (self,vstoreid ):
+        api = 'grid/vstore/remove'
+        auth_api = self._initurl(api, {'vstoreid': vstoreid })
+        print 'Stop vStore API ', auth_api
+        #print 'headers -- ', self._get_authheader()
+        resp = requests.post(auth_api,  headers=self._get_authheader())
+        if resp.status_code != 200:
+            print resp
+            #     This means something went wrong.
+            raise ApiError(resp.status_code,'POST {} / {}'.format(api, resp.status_code))
+        
+        resp_obj = resp.json()
+        
+        # pretty printing of json-formatted string
+        print json.dumps(resp_obj, sort_keys=True, indent=4)
+        
+        return resp_obj
+    
+    def expand_vstore_post (self,vstoreid,newsize,maxsize ):
+        api = 'grid/vstore/expand'
+        auth_api = self._initurl(api, {'vstoreid': vstoreid,'newsize':newsize, 'maxsize':maxsize  })
+        print 'Expand vStore API ', auth_api
+        #print 'headers -- ', self._get_authheader()
+        resp = requests.post(auth_api,  headers=self._get_authheader())
+        if resp.status_code != 200:
+            print resp
+            #     This means something went wrong.
+            raise ApiError(resp.status_code,'POST {} / {}'.format(api, resp.status_code))
+        
+        resp_obj = resp.json()
+        
+        # pretty printing of json-formatted string
+        print json.dumps(resp_obj, sort_keys=True, indent=4)
+        
+        return resp_obj
+    
+    
+    def create_vstore_post(self,vStoreName, vPoolID,sizeGB):
+        api = 'grid/vstore/create'
+        auth_api = self._initurl(api, {'vStoreName' : vStoreName, 'vpoolid': vPoolID, 'sizegb':sizeGB })
         print 'Create vPool API ', auth_api
         #print 'headers -- ', self._get_authheader()
         resp = requests.post(auth_api,  headers=self._get_authheader())
@@ -111,12 +147,10 @@ class GSAPI_vPools(GSAPI_Base):
         
         return resp_obj
     
-    
-    def update_vpool_post(self,vPoolID,name,optimizationsetting , qossetting, protectionsetting , grouptype ):
-        api = 'grid/vpool/update'
-        auth_api = self._initurl(api, {'VPoolID': vPoolID, 'name': name,'optimizationsetting':optimizationsetting,
-                                       'qossetting':qossetting,'protectionsetting':protectionsetting,'grouptype':grouptype })
-        print 'Update vPool API ', auth_api
+    def rename_vstore_post(self,vStoreName, vStoreID):
+        api = 'grid/vstore/rename'
+        auth_api = self._initurl(api, {'vStoreName' : vStoreName, 'vstoreid': vStoreID})
+        print 'Rename vPool API ', auth_api
         #print 'headers -- ', self._get_authheader()
         resp = requests.post(auth_api,  headers=self._get_authheader())
         if resp.status_code != 200:
@@ -131,53 +165,19 @@ class GSAPI_vPools(GSAPI_Base):
         
         return resp_obj
     
-    
-    
-    def rebalance_vpool_post(self,vPoolID):
-        api = 'grid/vpool/rebalance'
-        auth_api = self._initurl(api, {'vpoolid': vPoolID })
-        print 'Rebalance vPool API ', auth_api
-        #print 'headers -- ', self._get_authheader()
-        resp = requests.post(auth_api,  headers=self._get_authheader())
-        if resp.status_code != 200:
-            print resp
-            #     This means something went wrong.
-            raise ApiError(resp.status_code,'POST {} / {}'.format(api, resp.status_code))
-        
-        resp_obj = resp.json()
-        
-        # pretty printing of json-formatted string
-        print json.dumps(resp_obj, sort_keys=True, indent=4)
-        
-        return resp_obj
-    
-    
-    
-    def host_get (self):
-        api = 'grid/host/get'
-        auth_api = self._initurl(api)
-        print 'getting host ', auth_api
-        #print 'headers -- ', self._get_authheader()
-        resp = requests.get(auth_api,  headers=self._get_authheader())
-        if resp.status_code != 200:
-            print resp
-            #     This means something went wrong.
-            raise ApiError(resp.status_code, 'POST {} / {}'.format(api, resp.status_code))
-        
-        resp_obj = resp.json()
-         # pretty printing of json-formatted string
-        print json.dumps(resp_obj, sort_keys=True, indent=4)
-        
-        return resp_obj
-       
-if __name__ == "__main__":
-    api = GSAPI_vPools('10.100.37.153', '12309')
+    def __init__(self, host, port):
+        '''
+        Constructor
+        '''
+        GSAPI_Base.__init__(self, host, port)
+
+if __name__ == '__main__':
+    api = GSAPI_vSstores('10.100.37.153', '12309')
     print 'GS API Login - ', api.login()
-    #count = api.vpool_count()
-    #nodes =api.vpool_get();
-
-    nodes = api.host_get()
+    #"VStoreID": "6cf69b6c-a1aa-4c71-b091-62219ccbd083"
+    nodes = api.expand_vstore_post('1214f492-ab80-4cf7-b7c2-823a734b09e0', 2048, 0)
+    """
     for item in nodes :
-        api.vStore_per_host_get(item['NodeID'])
-
+        api.hosts_per_vstore_get(item['VStoreID'])
+    """
     print 'GS API Logout - ', api.logout()
